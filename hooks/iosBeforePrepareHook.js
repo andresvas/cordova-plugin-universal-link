d93ac6803dee33a46afef9f21b109da05ffb5e2c
Andres Felipe Vasquez Saldarriaga <avasquez@todo1.com>
2019-07-03 15:17:55 -0500

    new commit

--- hooks/iosBeforePrepareHook.js
+++ hooks/iosBeforePrepareHook.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 var ConfigXmlHelper = require('./lib/configXmlHelper.js');
 
 module.exports = function(ctx) {
-  run(ctx);
+    run(ctx);
 };
 
 /**
@@ -18,30 +18,30 @@ module.exports = function(ctx) {
  * @param {Object} ctx - cordova context object
  */
 function run(ctx) {
-  var projectRoot = ctx.opts.projectRoot;
-  var iosProjectFilePath = path.join(projectRoot, 'platforms', 'ios');
-  var configXmlHelper = new ConfigXmlHelper(ctx);
-  var newProjectName = configXmlHelper.getProjectName();
+    var projectRoot = ctx.opts.projectRoot;
+    var iosProjectFilePath = path.join(projectRoot, 'platforms', 'ios');
+    var configXmlHelper = new ConfigXmlHelper(ctx);
+    var newProjectName = configXmlHelper.getProjectName();
 
-  var oldProjectName = getOldProjectName(iosProjectFilePath);
+    var oldProjectName = getOldProjectName(iosProjectFilePath);
 
-  // if name has not changed - do nothing
-  if (oldProjectName.length && oldProjectName === newProjectName) {
-    return;
-  }
+    // if name has not changed - do nothing
+    //if (oldProjectName.length && oldProjectName === newProjectName) {
+    //  return;
+    //}
 
-  console.log('Project name has changed. Renaming .entitlements file.');
+    console.log('Project name has changed. Renaming .entitlements file.');
 
-  // if it does - rename it
-  var oldEntitlementsFilePath = path.join(iosProjectFilePath, oldProjectName, 'Resources', oldProjectName + '.entitlements');
-  var newEntitlementsFilePath = path.join(iosProjectFilePath, oldProjectName, 'Resources', newProjectName + '.entitlements');
+    // if it does - rename it
+    var oldEntitlementsFilePath = path.join(iosProjectFilePath, oldProjectName, 'Resources', oldProjectName + '.entitlements');
+    var newEntitlementsFilePath = path.join(iosProjectFilePath, oldProjectName, 'Resources', newProjectName + '.entitlements');
 
-  try {
-    fs.renameSync(oldEntitlementsFilePath, newEntitlementsFilePath);
-  } catch (err) {
-    console.warn('Failed to rename .entitlements file.');
-    console.warn(err);
-  }
+    try {
+        fs.renameSync(oldEntitlementsFilePath, newEntitlementsFilePath);
+    } catch (err) {
+        console.warn('Failed to rename .entitlements file.');
+        console.warn(err);
+    }
 }
 
 // region Private API
@@ -54,21 +54,21 @@ function run(ctx) {
  * @return {String} old project name
  */
 function getOldProjectName(projectDir) {
-  var files = [];
-  try {
-    files = fs.readdirSync(projectDir);
-  } catch (err) {
-    return '';
-  }
-
-  var projectFile = '';
-  files.forEach(function(fileName) {
-    if (path.extname(fileName) === '.xcodeproj') {
-      projectFile = path.basename(fileName, '.xcodeproj');
+    var files = [];
+    try {
+        files = fs.readdirSync(projectDir);
+    } catch (err) {
+        return '';
     }
-  });
 
-  return projectFile;
+    var projectFile = '';
+    files.forEach(function(fileName) {
+        if (path.extname(fileName) === '.xcodeproj') {
+            projectFile = path.basename(fileName, '.xcodeproj');
+        }
+    });
+
+    return projectFile;
 }
 
 // endregion
